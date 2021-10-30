d1798737c2a2cd36a645856cdac7aaa798312995
cantudo <45332456+cantudo@users.noreply.github.com>
2021-10-30 23:17:37 +0000

    Deploying to pages from @ cantudo/cantudo.github.io@50292ec30cacdd699f5d7c95c40138c506338f22 🚀

---
+++ _next/static/2Gj3tALa6koqdF4MKNr_L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/43508e955367a8f3.css","static/chunks/pages/index-acc5172a72f5402d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
