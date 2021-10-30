113c1154b97f53c7836008d5e848d46e82ec03cc
Antonio Cantudo <antoniokantudo@gmail.com>
2021-10-30 23:49:34 +0200

    built

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.pageBorder}></div> */}
+      <div className={styles.basicGrid + " place-items-center\
+                                           w-12/12 h-12/12\
+                                           text-white text-4xl\
+                                           font-mono font-bold"}>
+        <a className="hover:underline" href="/stquake">stquake</a>
+        <a className="hover:underline" href="/gru">gru</a>
+      </div>
+    </div>
+  )
+}
+
+export default Home
